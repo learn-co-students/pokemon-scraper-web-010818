1ffcc4cc8fb4993e5f277927f830c342339759ea
cnlucke <cristy.lucke@gmail.com>
2018-01-16 14:40:37 -0500

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,28 @@
+require 'pry'
+
+
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(args)
+    @id = args[:id] if args[:id]
+    @name = args[:name] if args[:name]
+    @type = args[:type] if args[:type]
+    @db = args[:db] if args[:db]
+  end
+
+  # Pokemon.save("Pikachu", "electric", @db)
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
+  end
+
+# Pokemon .find finds a pokemon from the database by their id number and returns a new Pokemon object
+  def self.find(id, db)
+    data = []
+    data = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
+    data = data[0]
+    new(id: data[0], name: data[1], type: data[2], db: db)
+  end
 end
+
+# Pry.start
